bb235e416235e2bcc26cd946fae6e8be3073d2a9
Sai Prakash Gundemeda <113116997+Saiprakashgundemeda@users.noreply.github.com>
2024-02-10 14:54:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
